bc5a0999d81011352e9c5f38cbdc78b14bb0df2b
a7medma7mouda7moud-design <a7medma7mouda7moud@gmail.com>
2026-08-07 04:02:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ if uploaded_files:
 
       try:
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-2.0-flash",
+
             contents=[img, prompt],
             config=types.GenerateContentConfig(
                 response_mime_type="application/json", temperature=0.2
